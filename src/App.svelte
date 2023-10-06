45ab3263803d1411357a68831d939b9faf2f7d97
Johannes Pahle <joe@jo-dev.de>
2023-10-06 09:51:11 +0200

    ✨ add header

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-  var text =
-    "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32.";
+  import Header from "./lib/Header.svelte";
+
+  var text = "";
 </script>
 
 <main>
+  <Header />
   <div class="output">
     {#each text.split(" ") as t}
       <b>{t.substring(0, t.length / 2 + 1)}</b>{t.substring(
@@ -18,9 +20,10 @@
 
 <style>
   .output {
-    text-align: center;
+    text-align: left;
+    font-size: 1.2rem;
     padding: 5rem;
-    padding-inline: 5vw;
+    padding-inline: 2vw;
   }
 
   b {
@@ -28,6 +31,6 @@
   }
   textarea {
     width: 70vw;
-    height: 40vh;
+    min-height: 20vh;
   }
 </style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>bionic reading</h1>
+</main>
+
+<style>
+  h1 {
+    font-size: 3rem;
+    font-weight: 900;
+    background: linear-gradient(45deg, #c61743 0%, #45ce7e 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    margin-bottom: 0;
+  }
+</style>
